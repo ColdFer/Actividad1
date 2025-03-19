8e593430ad2dfcdf1f310d6c7c7a5675c93d2342
fernando <fernando_mabar@hotmail.com>
2025-03-19 19:44:12 -0400

    primera presentacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'acercade', component: AcercadeComponent },
+  { path: 'productos', component: ProductosComponent },
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
